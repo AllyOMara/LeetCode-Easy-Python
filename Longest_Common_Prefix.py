45566f9af23eb074953d15f69200fe7b2e17410d
AllyOMara <allystellaomara@gmail.com>
2026-04-27 15:50:07 +1000

    Complete Longest_Common_Prefix.py

--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -14,31 +14,41 @@ class Solution(object):
         def find_prefix(strs):
             
             smallest_len = None
+            ans = []
+
             for string in strs:
                 str_len = len(string)
                 if smallest_len == None or smallest_len > str_len:
                     smallest_len = str_len
-
-            if strs == []:
+            
+            # Base cases
+            if smallest_len == 0:
+                return ans
+            elif strs == []:
                 return ""
             elif len(strs) == 1:
-                return strs
-            ans = []
-            for string in strs:
-                if string == "":
-                    return ""
+                for letter in strs[0]:
+                    ans.append(letter)
+                return ans
+
+            # Solving
             for i in range(smallest_len):
                 letter = strs[0][i]
                 for string in strs:
                     if string[i] != letter:
                         return ans
                 ans.append(letter)
+
+            return ans
+
+        # Converting solution into correct format
         ans = find_prefix(strs)
         ans_str = ""
 
         if ans == []:
             return ""
-        
-        for letter in ans:
-            ans_str = ans_str + letter
+        else:
+            for letter in ans:
+                ans_str = ans_str + letter
+
         return ans_str
